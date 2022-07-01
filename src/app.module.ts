82c3c2fa9c46912eafaa842bda77d5cd347bca48
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-07-01 15:49:06 +0300

    Finished server. Got Error, the project is dropped for some time.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './todos/app.controller';
-// import { AppService } from './app.service';
+import {TodoModule} from "./todos/todo.module";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeConfigService} from "./config/sequelizeConfig.service";
+import {databaseConfig} from "./config/configuration";
 
 @Module({
-  imports: [],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [
+      SequelizeModule.forRootAsync({
+        imports:[ConfigModule],
+        useClass:SequelizeConfigService,
+      }),
+      ConfigModule.forRoot(
+        {load: [databaseConfig]}
+      ),
+      TodoModule],
 })
 export class AppModule {}
